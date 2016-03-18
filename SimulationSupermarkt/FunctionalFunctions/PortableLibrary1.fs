f485ab447f6094858600f79bd1529715a4794c87
Hoye <hoyelam96@gmail.com>
2016-03-18 14:52:33 +0100

    added classes in C#

--- SimulationSupermarkt/FunctionalFunctions/PortableLibrary1.fs
+++ SimulationSupermarkt/FunctionalFunctions/PortableLibrary1.fs
@@ -2,3 +2,7 @@
 
 type Class1() = 
     member this.X = "F#"
+
+type Persoon() =
+    member this.Budget = 100
+    member this.Class = Class1()
--- SimulationSupermarkt/SimulationSupermarkt/Program.cs
+++ SimulationSupermarkt/SimulationSupermarkt/Program.cs
@@ -18,6 +18,45 @@ namespace SimulationSupermarkt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Persoon p1 = new Persoon();
+            setDefaultsPerson(p1);
+
+        }
+
+        class Persoon
+        {
+            public int budget { get; set; }
+            public Product[] product { get; set; }
+            public int movement { get; set; }
+        }
+
+        class Product
+        {
+            public string naam { get; set; }
+            public int prijs { get; set; }
+        }
+
+        class Store
+        {
+            public double revenue { get; set; }
+        }
+
+        class Point
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public Product[] itemVak { get; set; }
+            public int id { get; set; }
+        }
+
+        /// <summary>
+        /// spawn 
+        /// </summary>
+        /// <param name="customer"></param>
+        public static void setDefaultsPerson(Persoon customer)
+        {
+            customer.budget = 100;
+            customer.movement = 5;
         }
     }
 }
