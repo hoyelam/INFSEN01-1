5259b564ee313158b342ba6c335c81a9bb1a9ec3
Hoye <hoyelam96@gmail.com>
2016-03-18 14:18:10 +0100

    added F#

---
+++ SimulationSupermarkt/FunctionalFunctions/PortableLibrary1.fs
@@ -0,0 +1,4 @@
+﻿namespace FunctionalFunctions
+
+type Class1() = 
+    member this.X = "F#"
--- SimulationSupermarkt/SimulationSupermarkt/Program.cs
+++ SimulationSupermarkt/SimulationSupermarkt/Program.cs
@@ -18,7 +18,6 @@ namespace SimulationSupermarkt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            
         }
     }
 }
