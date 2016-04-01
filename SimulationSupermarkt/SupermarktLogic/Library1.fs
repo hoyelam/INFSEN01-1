fd38fd9588b297b16e9ab28c784d86465e2cec79
Hoye <hoyelam96@gmail.com>
2016-04-01 11:52:39 +0200

    removed FunctionalFunctions created SupermarktLogic

---
+++ SimulationSupermarkt/SupermarktLogic/Library1.fs
@@ -0,0 +1,44 @@
+﻿module SupermarktLogic
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Input
+
+
+type List<'a> = 
+  | Empty 
+  | Node of 'a * List<'a>
+let (<<) x xs = Node(x,xs)
+
+type SectionItem =
+    {
+        Position:    Vector2
+        Category:    string
+    }
+
+type Item = 
+    {
+        Category:    string
+        Price:       int
+    }
+
+type Customer = 
+    {
+        Position:    Vector2
+        Bag:         List<Item>
+        Velocity:    Vector2
+        Money:       int
+    }
+
+type Register =
+    {
+        Position:    Vector2
+        Cash:        int
+    }
+
+type GameState =
+    {
+        Customers:   List<Customer>
+        Registers:   List<Register>
+        SectionItems: List<SectionItem>
+    }
+
